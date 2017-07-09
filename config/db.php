4bbfd2fb7838ca5db46ba4938229229818f1c49e
Aarón Puche <aaronpuche92@gmail.com>
2017-07-09 17:25:18 +0200

    Subida de los archivos del TFG

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db688179526.db.1and1.com;dbname=db688179526',
+    'username' => 'dbo688179526',
+    'password' => '852654*Asd?',
+    'charset' => 'utf8',
+];
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: http://perretes-y-gatetes.es/web/');
+?>
