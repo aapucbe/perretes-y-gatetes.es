0a6847ce71e0712927109afd4d02e924b2af563b
Aarón Puche <aaronpuche92@gmail.com>
2017-07-16 20:39:36 +0200

    Pading formulario index

--- views/site/index.php
+++ views/site/index.php
@@ -6,45 +6,35 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Perretes y Gatetes';
 ?>
-<div class="site-index">
-    <div class="container div-top">
-        <div class="row">
-            <div class="col-lg-6 div-center img-index">
-                <img src="../web/images/perro-y-gato.jpg" class="img-rounded"/>
-            </div>
-            <div class="col-lg-6 div-center border-box">
-                <h1>¡Regístrate!</h1>
-                <h3>Sólo te llevará un minuto</h3>
-                <br>
-                <div class="row">
-                    <?php $form = ActiveForm::begin([
-                        'id' => 'register-form',
-                        'layout' => 'horizontal',
-                        /*'fieldConfig' => [
-                            'labelOptions' => ['class' => 'col-md-1 control-label'],
-                        ],*/
-                    ]); ?>
-
-                        <?= $form->field($model, 'nombre')->textInput() ?>
-
-                        <?= $form->field($model, 'apellidos')->textInput() ?>
-
-                        <?= $form->field($model, 'email')->textInput() ?>
-
-                        <?= $form->field($model, 'password')->passwordInput() ?>
-
-                        <?= $form->field($model, 'repeat_password')->passwordInput() ?>
-
-                        <div class="form-group">
-                            <div class="col-lg-offset-1 col-lg-11">
-                                <?= Html::submitButton('Registrar', ['class' => 'btn btn-primary', 'name' => 'register-button']) ?>
-                            </div>
-                        </div>
-
-                    <?php ActiveForm::end(); ?>
-                </div>
-            </div>
-        </div>
+<div class="site-index div-top">
+    <div class="col-lg-6 div-center img-index">
+        <img src="../web/images/perro-y-gato.jpg" class="img-rounded"/>
     </div>
+    <div class="col-lg-6 div-center border-box">
+        <h1>¡Regístrate!</h1>
+        <h3>Sólo te llevará un minuto</h3>
+        <br>
+            <?php $form = ActiveForm::begin([
+                'id' => 'register-form',
+                'layout' => 'horizontal',
+            ]); ?>
 
+                <?= $form->field($model, 'nombre')->textInput() ?>
+
+                <?= $form->field($model, 'apellidos')->textInput() ?>
+
+                <?= $form->field($model, 'email')->textInput() ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+                <?= $form->field($model, 'repeat_password')->passwordInput() ?>
+
+                <div class="form-group">
+                    <div class="col-lg-offset-1 col-lg-11">
+                        <?= Html::submitButton('Registrar', ['class' => 'btn btn-primary', 'name' => 'register-button']) ?>
+                    </div>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+    </div>
 </div>
